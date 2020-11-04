61eb376db96a0c998201e22be2bd4d8198e9a865
jazmin0226 <jariass@ucenfotec.ac.cr>
2020-11-03 23:13:08 -0600

    Cross Origin Config
    El CORS agregado y funcionando

--- final-project-js/db/index.js
+++ final-project-js/db/index.js
@@ -29,6 +29,15 @@ retryWrites: false
 .then(() => console.log('Connection to CosmosDB successful'))
 .catch((err) => console.error(err));
 
+// CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 //Router
 app.use('/', components);
 
